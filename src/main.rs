2e2752fbccf8b104111cd272afcba3a1aebe3733
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-10-28 18:50:08 +0200

    Generating receipts message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, sync::Arc};
 
-use keri::database::sled::SledEventDatabase;
+use hosting_service::witness::Witness;
 use serde::{Deserialize, Serialize};
 use warp::Filter;
 
@@ -11,7 +11,7 @@ pub struct Message {
 
 #[tokio::main]
 async fn main() {
-    let db = Arc::new(SledEventDatabase::new(Path::new("./db")).unwrap()); //Arc::new(Mutex::new(Witness::new(Path::new("./db"))));
+    let db = Arc::new(Witness::new()); //Arc::new(Mutex::new(Witness::new(Path::new("./db"))));
 
     let api = filters::all_filters(db);
 
@@ -21,27 +21,27 @@ async fn main() {
 mod filters {
     use std::sync::Arc;
 
-    use hosting_service::witness;
-    use keri::{database::sled::SledEventDatabase, prefix::IdentifierPrefix};
+    use hosting_service::witness::{Witness};
+    use keri::{prefix::IdentifierPrefix};
     use warp::Filter;
 
     use crate::{json_body, Message};
 
     pub fn all_filters(
-        db: Arc<SledEventDatabase>,
+        db: Arc<Witness>,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         publish(db.clone()).or(get_kel(db.clone()))
     }
 
     // POST /publish with JSON body
     pub fn publish(
-        db: Arc<SledEventDatabase>,
+        db: Arc<Witness>,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::path("publish")
             .and(json_body())
             .and(warp::any().map(move || db.clone()))
-            .map(move |param: Message, wit: Arc<SledEventDatabase>| {
-                match witness::process(wit, &param.body) {
+            .map(move |param: Message, wit: Arc<Witness>| {
+                match wit.process(&param.body) {
                     Ok(_) => {
                         return Ok("Success".to_string());
                     }
@@ -54,15 +54,15 @@ mod filters {
 
     // GET/kel/{identifier}
     pub fn get_kel(
-        db: Arc<SledEventDatabase>,
+        db: Arc<Witness>,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::path("kel")
             .and(warp::path::param())
             .and(warp::any().map(move || db.clone()))
-            .map(move |identifier: String, wit: Arc<SledEventDatabase>| {
+            .map(move |identifier: String, wit: Arc<Witness>| {
                 let id: IdentifierPrefix = identifier.parse().unwrap();
 
-                let kel = String::from_utf8(witness::resolve(wit, &id).unwrap().unwrap()).unwrap();
+                let kel = String::from_utf8(wit.resolve(&id).unwrap().unwrap()).unwrap();
                 Ok(warp::reply::json(&kel))
             })
     }
--- src/witness.rs
+++ src/witness.rs
@@ -1,29 +1,69 @@
 
-use std::sync::Arc;
+use std::{path::Path, sync::Arc};
 
-use keri::{
-    database::sled::SledEventDatabase, event_message::parse::signed_message,
-    prefix::IdentifierPrefix, processor::EventProcessor,
-};
+use keri::{database::sled::SledEventDatabase, derivation::{basic::Basic, self_addressing::SelfAddressing, self_signing::SelfSigning}, event::{Event, SerializationFormats, event_data::{EventData, Receipt}}, event_message::{parse::{Deserialized, signed_message}, signed_event_message::SignedNontransferableReceipt}, keys::{PrivateKey, PublicKey}, prefix::{BasicPrefix, IdentifierPrefix}, processor::EventProcessor };
+use rand::rngs::OsRng;
 
 type Result<T> = std::result::Result<T, keri::error::Error>;
 
-pub fn process(db: Arc<SledEventDatabase>, stream: &str) -> Result<()> {
-    let (_rest, message) = signed_message(&stream.as_bytes()).unwrap();
-    let processor = EventProcessor::new(Arc::clone(&db));
+pub struct Witness {
+    keypair: (PrivateKey, PublicKey),
+    prefix: BasicPrefix,
+    db: Arc<SledEventDatabase>
+}
 
-    processor.process(message)?;
+impl Witness {
+
+    pub fn new() -> Self {
+        let kp = ed25519_dalek::Keypair::generate(&mut OsRng {});
+        let (vk, sk) = (kp.public, kp.secret);
+        let vk = PublicKey::new(vk.to_bytes().to_vec());
+        let sk = PrivateKey::new(sk.to_bytes().to_vec());
+        let keypair = (sk, vk.clone());
+        let prefix = Basic::Ed25519.derive(vk);
+        let db = Arc::new(SledEventDatabase::new(Path::new("./db")).unwrap());
+        Self {keypair, prefix, db}
+    }
+pub fn process(&self, stream: &str) -> Result<()> {
+    let (_rest, message) = signed_message(&stream.as_bytes()).unwrap();
+    let processor = EventProcessor::new(Arc::clone(&self.db));
+
+    processor.process(message.clone())?;
 
     // TODO Create witness receipt and add it to db
+    let ser = stream.as_bytes();
+    if let Deserialized::Event(ev) = message {
+        let signature = self.keypair.0.sign_ed(&ser)?;
+        let rcp = Event {
+            prefix: ev.deserialized_event.event_message.event.prefix,
+            sn: ev.deserialized_event.event_message.event.sn,
+            event_data: EventData::Rct(Receipt {
+                receipted_event_digest: SelfAddressing::Blake3_256.derive(&ser),
+            }),
+        }
+        .to_message(SerializationFormats::JSON)?;
 
+        let signature = SelfSigning::Ed25519Sha512.derive(signature) ;
+        
+        let signed_rcp = SignedNontransferableReceipt::new(&rcp, vec![(self.prefix.clone(), signature)]);
+
+        println!("rcp: {}", String::from_utf8(signed_rcp.serialize().unwrap()).unwrap());
+
+        processor
+            .process(signed_message(&signed_rcp.serialize()?).unwrap().1)?;
+
+    }
     Ok(())
 }
 
-pub fn resolve(db: Arc<SledEventDatabase>, id: &IdentifierPrefix) -> Result<Option<Vec<u8>>> {
-    let processor = EventProcessor::new(Arc::clone(&db));
+pub fn resolve(&self, id: &IdentifierPrefix) -> Result<Option<Vec<u8>>> {
+    let processor = EventProcessor::new(Arc::clone(&self.db));
+    let t = processor.get_kerl(id);
+    println!("t: {}", String::from_utf8(t.unwrap().unwrap()).unwrap());
     processor.get_kerl(id)
 }
 
-pub fn get_receipts(db: Arc<SledEventDatabase>, id: &IdentifierPrefix) -> Result<Option<Vec<u8>>> {
+pub fn get_receipts(&self, id: &IdentifierPrefix) -> Result<Option<Vec<u8>>> {
     todo!()
+}
 }
